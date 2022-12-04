ee12522733d17589f79c3a003f5747e56f8d1c9f
Christian J. Rodriguez <93481778+christian-j174@users.noreply.github.com>
2022-12-04 10:58:04 -0400

    fix constructor

--- erc1155-contract/contracts/AwesomeGame.sol
+++ erc1155-contract/contracts/AwesomeGame.sol
@@ -3,18 +3,12 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract AwesomeGame is ERC1155 {
-    uint256 public constant GOLD = 0;
-    uint256 public constant SILVER = 1;
-    uint256 public constant SWORD = 2;
-    uint256 public constant SHIELD = 3;
-    uint256 public constant CROWN = 4;
+contract Receipt is ERC1155 {
+    uint256 public constant ID = 0;
+    uint256 public constant REQUEST_ID = 1;
 
     constructor() ERC1155("https://awesomegame.com/assets/{id}.json") {
-        _mint(msg.sender, GOLD, 10**18, "");
-        _mint(msg.sender, SILVER, 10**18, "");
-        _mint(msg.sender, SWORD, 1000, "");
-        _mint(msg.sender, SHIELD, 1000, "");
-        _mint(msg.sender, CROWN, 1, "");
+        _mint(msg.sender, ID, 10**18, "");
+        _mint(msg.sender, REQUEST_ID, 10**18, "");
     }
 }
