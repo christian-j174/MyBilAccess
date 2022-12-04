51846d5660c83faa0b06e397c55613c3f6732f0d
Christian J. Rodriguez <93481778+christian-j174@users.noreply.github.com>
2022-12-04 13:03:31 -0400

    new inputs

--- main.py
+++ main.py
@@ -10,12 +10,27 @@ json1 = {
   "Size": 8,
 }
 
+entity = input("Position in Company or Goverment: ")
+name2 = input("First and Last Name: ")
+category2 = input("Project Category: ")
+projectName2 = input("Project Name: ")
+
+#TEST CASE INFORMATION
+json1 = {
+  "Entity": entity,
+  "name": name2,
+  "category": category2,
+  "quotes": [25000, 12000, 30000],
+  "Size": 8,
+}
+
+
 def smartContract(json1):
   verification_Code = ''.join((secrets.choice(string.ascii_letters) for i in range(json1["Size"]))).upper()
   return f"Your Validation Code is:  {verification_Code}"
 
 
-# print(code1)
+
 
 if __name__ == '__main__':
   user1 = smartContract(json1)
